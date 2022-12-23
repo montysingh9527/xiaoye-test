efcb3e00f2dc5a6e6fc7d0b9f75720483b94ba3c
marvin <xiaomarvin9527@gmail.com>
2022-12-24 01:15:43 +0800

    js—LeetCode题解

--- src/pages/leetcode/leetcode.js
+++ src/pages/leetcode/leetcode.js
@@ -41,7 +41,6 @@ function sumne2(nums, target) {
 }
 // console.log(sumne2([1,4,6,8,9],12))
 
-
 /**
  * 题目二、
  * 找出每一项都包含的字符
@@ -71,7 +70,6 @@ function longestCommonPrefix(strs) {
 }
 // console.log(longestCommonPrefix(["flieo","fleoe","flomeo","fpoeo"]))
 
-
 /**
  * 题目三、
  * 找出needle出现在haystack中的初始索引
@@ -92,3 +90,38 @@ function strStr(haystack, needle) {
   return -1;
 }
 // console.log(strStr("hello", "lo"))
+
+/**
+ * 3 . 无重复字符的最长子串
+ * 示例1："abcabcbb"   // 3
+ * 示例2："bbbbb"   // 1
+ * 示例3："keewp"   // 3
+ */
+function lengthOfLongestSubstring(s) {
+  // 创建一个set
+  const set = new Set();
+  // 创建两个指针，第一个指向字符串的开头-j，第二个随着for循环遍历字符串-i
+  let i = 0,
+    j = 0,
+    maxLength = 0;
+  if (s.length === 0) {
+    return 0;
+  }
+  for (i; i < s.length; i++) {
+    // 如果set里没有s[i]，说明目前为止还没有重复的字符，把s[i]添加到set里，然后更新最大不重复字符的数量
+    if (!set.has(s[i])) {
+      set.add(s[i]);
+      maxLength = Math.max(maxLength, set.size);
+    } else {
+      // 如果set里有s[i]，则从set里开始删除s[j]，并且递增j，在检查set里是否有s[i]，直到set里没有s[i]为止
+      while (set.has(s[i])) {
+        set.delete(s[j]);
+        j++;
+      }
+      set.add(s[i]);
+    }
+  }
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
